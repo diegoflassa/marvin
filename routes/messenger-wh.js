8b4cd6d6235604434cfd735636b3125c28ccdca8
Diego Felipe Lassa <diegoflassa@gmail.com>
2018-04-27 13:12:55 -0300

    -Disabled method for entities handling

--- routes/messenger-wh.js
+++ routes/messenger-wh.js
@@ -108,8 +108,9 @@ function handleMessage(sender_psid, received_message) {
   // Checks if the message contains text
   if (received_message.text) {    
 
-    //IDentfy the message´s entities
-    handleEntities(received_message.nlp);
+    //Identfy the message´s entities
+    //TODO:  Check for Bug
+    //handleEntities(received_message.nlp);
 
     // Create the payload for a basic text message, which
     // will be added to the body of our request to the Send API
